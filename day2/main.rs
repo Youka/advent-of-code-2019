8664a46f65208fcfad71577ddd61d5c950d0cbee
Youka <spanknebel.borken@t-online.de>
2019-12-02 22:36:51 +0100

    day 2, part 2

--- day2/main.rs
+++ day2/main.rs
@@ -1,31 +1,53 @@
-fn main() {
-    use std::io::{stdin,BufRead};
-    // Read input as integer list
-    let mut intcode = stdin().lock()
-        .split(b',')
-        .map(|result_token| result_token.expect("Expected token of comma-separated input!"))
-        .map(|token| String::from_utf8(token).expect("Input token isn't a valid UTF-8 string!"))
-        .map(|string_token| string_token.parse().expect("Input token isn't an unsigned integers!"))
-        .collect::<Vec<usize>>();
-    println!("Intcode input: {:?}", intcode);
-    // Restore "1202 program alarm"
-    *intcode.get_mut(1).expect("Intcode must have at least 2 elements!") = 12;
-    *intcode.get_mut(2).expect("Intcode must have at least 3 elements!") = 2;
+fn process_intcode(mut intcode: Vec<usize>, noun: usize, verb: usize) -> usize {
+    // Insert "noun" and "verb"
+    *intcode.get_mut(1).expect("Intcode must have at least 2 elements!") = noun;
+    *intcode.get_mut(2).expect("Intcode must have at least 3 elements!") = verb;
     // Go through opcodes + registers
-    for i in (0..intcode.len()).step_by(4) {
-        match intcode[i] {
+    for pos in (0..intcode.len()).step_by(4) {
+        match intcode[pos] {
+            // Halt program
             99 => break,
+            // Arithmetic operations
             opcode if (1..=2).contains(&opcode) => {
-                let out_reg = intcode[i+3];
-                match opcode {
-                    1 => intcode[out_reg] = intcode[intcode[i+1]] + intcode[intcode[i+2]],
-                    2 => intcode[out_reg] = intcode[intcode[i+1]] * intcode[intcode[i+2]],
+                let out_addr = intcode[pos+3];
+                intcode[out_addr] = match opcode {
+                    1 => intcode[intcode[pos+1]] + intcode[intcode[pos+2]],
+                    2 => intcode[intcode[pos+1]] * intcode[intcode[pos+2]],
                     _ => unreachable!()
                 }
             }
-            opcode => panic!("Opcode {} at position {} isn't supported!", opcode, i)
+            // Error!
+            opcode => panic!("Opcode {} at position {} isn't supported!", opcode, pos)
         }
     }
-    // Output processed intcode
-    println!("Intcode output: {:?}", intcode);
+    *intcode.first().expect("Intcode shouldn't be empty!")
+}
+
+fn puzzle_part1(intcode: &Vec<usize>) {
+    println!("[Part 1] Output: {}", process_intcode(intcode.clone(), 12, 2));
+}
+
+fn puzzle_part2(intcode: &Vec<usize>) {
+    for noun in 0..=99 {
+        for verb in 0..=99 {
+            if process_intcode(intcode.clone(), noun, verb) == 19690720_usize {
+                println!("[Part 2] Noun={} - Verb={} - Result={}", noun, verb, 100 * noun + verb);
+                return;
+            }
+        }
+    }
+}
+
+fn main() {
+    use std::io::{stdin,BufRead};
+    // Read input as integer list
+    let intcode = stdin().lock()
+        .split(b',')
+        .map(|result_token| result_token.expect("Expected token of comma-separated input!"))
+        .map(|token| String::from_utf8(token).expect("Input token isn't a valid UTF-8 string!"))
+        .map(|string_token| string_token.parse().expect("Input token isn't an unsigned integer!"))
+        .collect::<Vec<usize>>();
+    // Solve puzzles
+    puzzle_part1(&intcode);
+    puzzle_part2(&intcode);
 }
