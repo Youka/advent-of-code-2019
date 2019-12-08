f96424b969ebfc6b11e76a7d047263fbeec45b76
Youka <spanknebel.borken@t-online.de>
2019-12-08 17:55:55 +0100

    added kotlin for day 8

--- run.sh
+++ run.sh
@@ -6,8 +6,16 @@ mkdir -p out
 # Compile code and run executable
 build_and_run() {
     echo "##### Day $1 #####"
-    rustc day$1/main.rs --edition 2018 -O -v -o out/day$1 && \
-    cat day$1/input.txt | ./out/day$1
+    # Rust (https://www.rust-lang.org/tools/install)
+    if [ -f day$1/main.rs ]; then
+        rustc day$1/main.rs --edition 2018 -O -v -o out/day$1 && \
+        cat day$1/input.txt | ./out/day$1
+    fi
+    # Kotlin (https://github.com/JetBrains/kotlin/releases/latest)
+    if [ -f day$1/main.kt ]; then
+        kotlinc -include-runtime -jvm-target 1.8 -d out/day$1.jar day$1/main.kt
+        java -jar out/day$1.jar
+    fi
     echo "##################"
     echo
 }
@@ -19,4 +27,5 @@ build_and_run() {
 #build_and_run 4
 #build_and_run 5
 #build_and_run 6
-build_and_run 7
+#build_and_run 7
+build_and_run 8
